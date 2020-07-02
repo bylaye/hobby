85fa4b1b6f46d638f01c78b5710693a5a9bc06b1
Abdoulaye Niang <abdlynng@gmail.com>
2020-07-02 15:07:06 +0000

    ajout de la fonction de decryptage

--- crypto/cesar/cesar.py
+++ crypto/cesar/cesar.py
@@ -3,7 +3,7 @@ def cesar_chiffre_nombre (valeur, decalage):
     return (valeur+decalage)%26
 
 def cesar_dechiffre_nombre(valeur, decalage):
-    return (valeur+decalage)%26
+    return (valeur-decalage)%26
 
 def cesar_chiffre_message(message,decalage):
     message_crypte = []
@@ -15,3 +15,16 @@ def cesar_chiffre_message(message,decalage):
         message_crypte.append(caractere_crypte)
     message_crypte = "".join(message_crypte)
     return(message_crypte)
+
+def cesar_dechiffre_message (message, decalage):
+    message_decrypte = []
+    for caractere in message:
+        caractere = caractere.upper()
+        nb = ord(caractere) - 65 
+        nb_decrypte = cesar_dechiffre_nombre(nb, decalage)
+        caractere_decrypte = chr(nb_decrypte+65)
+        message_decrypte.append(caractere_decrypte)
+    message_decrypte = "".join(message_decrypte)
+    return (message_decrypte)
+
+print(cesar_dechiffre_message("CDFQWNCAGVPKCPI",2))
