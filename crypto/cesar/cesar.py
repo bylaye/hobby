19309a44647ea55866ee2232c6ef61730dd349c7
Abdoulaye Niang <abdlynng@gmail.com>
2020-07-04 11:36:34 +0000

    prise en charge des espaces

--- crypto/cesar/cesar.py
+++ crypto/cesar/cesar.py
@@ -8,10 +8,13 @@ def cesar_dechiffre_nombre(valeur, decalage):
 def cesar_chiffre_message(message,decalage):
     message_crypte = []
     for caractere in message: 
-        caractere = caractere.upper() # converti le caractere en majuscule
-        nb = ord(caractere) - 65 # converti caractere en nombre
-        nb_crypte = cesar_chiffre_nombre(nb, decalage)
-        caractere_crypte = chr(nb_crypte+65) # converti nombre en caractere
+        if caractere == " ":
+            caractere_crypte = caractere
+        else:
+            caractere = caractere.upper() # converti le caractere en majuscule
+            nb = ord(caractere) - 65 # converti caractere en nombre
+            nb_crypte = cesar_chiffre_nombre(nb, decalage)
+            caractere_crypte = chr(nb_crypte+65) # converti nombre en caractere
         message_crypte.append(caractere_crypte)
     message_crypte = "".join(message_crypte)
     return(message_crypte)
@@ -19,12 +22,16 @@ def cesar_chiffre_message(message,decalage):
 def cesar_dechiffre_message (message, decalage):
     message_decrypte = []
     for caractere in message:
-        caractere = caractere.upper()
-        nb = ord(caractere) - 65 
-        nb_decrypte = cesar_dechiffre_nombre(nb, decalage)
-        caractere_decrypte = chr(nb_decrypte+65)
+        #si le caractere est un espace on ne le change pas 
+        if caractere == " ":
+            caractere_decrypte = caractere
+        else:
+            caractere = caractere.upper()
+            nb = ord(caractere) - 65 
+            nb_decrypte = cesar_dechiffre_nombre(nb, decalage)
+            caractere_decrypte = chr(nb_decrypte+65)
+
         message_decrypte.append(caractere_decrypte)
     message_decrypte = "".join(message_decrypte)
     return (message_decrypte)
 
-print(cesar_dechiffre_message("CDFQWNCAGVPKCPI",2))
