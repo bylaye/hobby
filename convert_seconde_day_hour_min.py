56a8e72273c31ef8ad3cb64715eefb5ae2d092c5
Abdoulaye Niang <abdlynng@gmail.com>
2020-12-14 21:46:39 +0000

    formatage de l'affichage

--- convert_seconde_day_hour_min.py
+++ convert_seconde_day_hour_min.py
@@ -4,7 +4,8 @@ day = 0
 hour = 0
 minute = 0
 
-if (seconde > 0):
+if (seconde >= 0):
+	# conversion des secondes => jours: heures: minutes: seconde 
 	if (seconde >= 86400):
 		day = seconde // 86400
 		seconde %= 86400
@@ -14,6 +15,16 @@ if (seconde > 0):
 	if (seconde >= 60):
 		minute = seconde // 60
 		seconde %= 60
-	print (day,' d:',hour,' h:',minute,' m:',seconde,'s')
+	# formatage de l'affichage 
+	if len(str(day))<=1:
+			day = "0"+str(day)
+	if len(str(hour))<=1:
+			hour = "0"+str(hour)
+	if len(str(minute))<=1:
+			minute = "0"+str(minute)
+	if len(str(seconde))<=1:
+			seconde = "0"+str(seconde)
+	print (day,'d:',hour,'h:',minute,'m:',seconde,'s')
 else:
 	print("donner un nombre postif")
+	
